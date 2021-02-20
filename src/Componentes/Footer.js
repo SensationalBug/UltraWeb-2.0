8273f3709cb37ca4ce3134f4c3fff5399f19e933
breidydl@gmail.com <breidydl@gmail.com>
2021-02-20 09:53:38 -0400

    ya arregle los cambios de Choco

--- src/Componentes/Footer.js
+++ src/Componentes/Footer.js
@@ -8,20 +8,20 @@ export const Footer = () => {
   return (
     <section className="container-fluid footerSection py-3">
       <Row>
-        <Col className="text-md-left text-white col-md-9 ml-md-5 my-auto">
+        <Col className="text-md-left text-white col-md-8 col-sm-12 ml-md-5">
           <FontAwesomeIcon icon={faLaptopCode} size="4x" className="ml-md-5" />
           <span className="h2">UltraWeb</span>
           <p className="mx-4 col-md-4 py-1 footerMsg">
             Somos expertos en React, Desarrollo Web, Firebase!
           </p>
         </Col>
-        <Col className="text-left text-white">
-            <p className="mr-5 h3">Contáctanos</p>
+        <Col className="text-md-left text-sm-center text-white col-md-3 col-12">
+            <p className="mr-5 h3 contactFooterLabel">Contáctanos</p>
             <Col>
                 <MdEmail size="2em" />
                 <span>Breiydl@gmail.com</span>
             </Col>
-            <Col className="py-1">
+            <Col className="py-0">
                 <MdPhoneAndroid size="2em" />
                 <span>809-252-6353</span>
             </Col>
